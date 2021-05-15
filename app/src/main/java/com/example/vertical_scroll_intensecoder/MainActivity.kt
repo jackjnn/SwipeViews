fa09285d063cdaaf8c811444b738d3bb4fb5e0a9
jackjnn <jacknyangenj.jn2@gmail.com>
2021-05-15 14:47:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vertical_scroll_intensecoder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vertical_scroll_intensecoder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
